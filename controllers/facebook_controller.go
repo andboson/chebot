1eee93f1528213d45bbc1bc1e9ee0441961e8055
andboson <andboson@gmail.com>
2017-09-09 23:37:15 +0300

    added ctrlr

---
+++ controllers/facebook_controller.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"github.com/andboson/skypeapi"
+	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
+	"net/http"
+)
+
+func FacebookHook(c echo.Context) error {
+	var request skypeapi.Activity
+	err := c.Bind(&request)
+	if err != nil {
+		log.Printf("--- skype decode msg error!: %+v  >>%s", c.Request(), err)
+	}
+
+	resp := map[string]string{
+		"status": "success",
+	}
+
+	log.Printf("user --", request.From.Name, request.From.ID)
+
+	return c.JSON(http.StatusOK, resp)
+}
