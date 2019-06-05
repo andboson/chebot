07f540adab2379b276beb9f4fde5a606be15b2e1
andboson <andboson@gmail.com>
2019-06-05 21:28:52 +0300

    fixed option

--- routes/router.go
+++ routes/router.go
@@ -9,7 +9,9 @@ func Router() *echo.Echo {
 	e := echo.New()
 	e.POST("/ai.get_response", controllers.GetAiResponse)
 	e.POST("/skype.hook", controllers.SkypeHook)
-	e.OPTIONS("/skype.hook", controllers.SkypeHook)
+	e.OPTIONS("/skype.hook", func(c echo.Context) error{
+		return  c.NoContent(204)
+	})
 
 	return e
 }
